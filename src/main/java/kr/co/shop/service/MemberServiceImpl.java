df21e15d5788323f965abe2ecaf9b5113544ab0e
geoson <balsohn@naver.com>
2024-08-29 17:48:29 +0900

    cartview

--- src/main/java/kr/co/shop/service/MemberServiceImpl.java
+++ src/main/java/kr/co/shop/service/MemberServiceImpl.java
@@ -1,7 +1,10 @@
 package kr.co.shop.service;
 
+import java.time.LocalDate;
+import java.time.format.TextStyle;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -60,10 +63,9 @@ public class MemberServiceImpl implements MemberService {
 				for(int i=0;i<codes.length;i++) {
 					String pcode=codes[i].substring(0,12);
 					int su=Integer.parseInt(codes[i].substring(13));
-					System.out.println(pcode);
 					HashMap product=mapper.getProduct(pcode);
 					product.put("su", su);
-					System.out.println(product.get("title"));
+					
 					pMapAll.add(product);
 					
 				}
@@ -71,6 +73,42 @@ public class MemberServiceImpl implements MemberService {
 		} else {
 			String userid=session.getAttribute("userid").toString();
 			pMapAll=mapper.cartView(userid);
+		}
+		
+		if(pMapAll!=null) {
+			for(int i=0;i<pMapAll.size();i++) {
+				HashMap map=pMapAll.get(i);
+				
+				LocalDate today=LocalDate.now();
+				int baeday=Integer.parseInt(map.get("baeday").toString());
+				LocalDate xday=today.plusDays(baeday);
+				String yoil=xday.getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.KOREAN);
+				String baeEx=null;
+				if(baeday==1) {
+					baeEx="내일("+yoil+") 도착예정";
+				} else if(baeday==2) {
+					baeEx="모레("+yoil+") 도착예정";
+				} else {
+					int m=xday.getMonthValue();
+					int d=xday.getDayOfMonth();
+					baeEx=m+"/"+d+"("+yoil+") 도착예정";
+				}
+				// ArrayList<HashMap>에 baeEx넣어주기
+				map.put("baeEx", baeEx);
+				
+				// 2. 상품금액(할인율이 적용된 금액)
+				int price=Integer.parseInt(map.get("price").toString());
+				int halin=Integer.parseInt(map.get("halin").toString());
+				int su=Integer.parseInt(map.get("cart_su").toString());
+				int halinprice=(int)( price-(price*halin/100.0) )*su;
+			    map.put("halinprice", halinprice);
+			    
+				// 3. 적립금
+				int juk=Integer.parseInt(map.get("juk").toString());
+				int jukprice=(int)(price*juk/100.0)*su;
+				map.put("jukprice", jukprice);
+				
+			}
 		}
 		
 		model.addAttribute("pMapAll",pMapAll);
